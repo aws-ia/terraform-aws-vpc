bee5b3f8c9b404a98653fd36fcf83195871e6dbc
dawright22 <53329137+dawright22@users.noreply.github.com>
2020-08-25 13:57:27 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,14 +21,19 @@ resource "tfe_workspace" "quickstart-workspace" {
   organization = tfe_organization.quickstart-org.name
 }
 
-output "backend_file" {
+output "To_create_backend_file" {
   value = <<README
-# then run these commands in order:
+# Run these commands in order:
+#
 # 1. terraform output | cut -d '=' -f 2 >> ./VPC/backend.hcl.json
+#
+# Then to create your VPC run these commands:
+#
 # 2. cd ./VPC
 # 3. terraform init -backend-config=backend.hcl.json
-# 4. terraform apply to deploy your VPC
-
+$ 4. apply your AWS key and Secrets key to the terraform workspace created in step 1
+# 5. terraform apply to deploy your VPC
+$
 {
   "workspaces": [
     {
