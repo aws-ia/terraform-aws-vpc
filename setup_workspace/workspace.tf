0e9269675ff9d2b0ee6f199344c1f06fd4448e57
Tony Vattathil <avattathil@gmail.com>
2020-11-18 18:40:34 -0800

    Fix path to backend.hcl Update demo instructions

--- setup_workspace/workspace.tf
+++ setup_workspace/workspace.tf
@@ -22,7 +22,7 @@ module "tfc_workspace" {
 resource "null_resource" "setup_backend_file" {
   depends_on = [module.tfc_workspace]
   provisioner "local-exec" {
-    command = "mv backend.hcl ../deploy_demo/backend.hcl"
+    command = "mv backend.hcl ../deploy_demo"
   }
 }
 
@@ -30,7 +30,8 @@ resource "null_resource" "setup_backend_file" {
 resource "null_resource" "remote_init" {
   depends_on = [null_resource.setup_backend_file]
   provisioner "local-exec" {
-    command = "cd ../deploy_demo && terraform init -backend-config=backend.hcl"
+    working_dir = "../deploy_demo"
+    command     = "terraform init -backend-config=backend.hcl"
   }
 }
 
@@ -39,11 +40,14 @@ output "user_instructions" {
 # org name    = ${module.tfc_workspace.tfm-aws-org-name}
 # workspace   = ${module.tfc_workspace.tfm-aws-workspace-name}
 #
+#
 # Run these commands in order:
    cd ../deploy_demo
+
 # Configure your tfvars file
   AWS_SECRET_ACCESS_KEY = "*****************"
   AWS_ACCESS_KEY_ID     = "*****************"
+
 # !!!!CAUTION!!!!: Make sure your credential are secured ourside version control (and follow secrets mangement bestpractices)
 #   
    terraform apply  -var-file="/Users/username/.aws/terraform.tfvars"
