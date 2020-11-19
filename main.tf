e386499792d3626d82a6d6b1c0ac6a1d025eff26
Tony Vattathil <avattathil@gmail.com>
2020-11-18 16:52:34 -0800

    Add root module

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@
 # Defaults
 ######################################
 terraform {
-  required_version = ">= 0.13"
-  
+  required_version = ">= 0.13.5"
+
   backend "remote" {}
 }
 
 provider "aws" {
-  region = "ap-southeast-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "name" {
-  prefix = "aws-quickstart"
+  prefix = "tfm-aws"
   length = 1
 }
 
@@ -25,23 +25,23 @@ resource "random_pet" "name" {
 ######################################
 
 module "quickstart_vpc" {
-  source = "../modules/quickstart_vpc"
-  region = "ap-southeast-2"
-  name   = "${random_pet.name.id}"
-  cidr     = "10.0.0.0/16"
-  public_subnets      = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20", "10.0.240.0/22", "10.0.244.0/22"]
-  private_subnets_A   = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19", "10.0.232.0/22", "10.0.236.0/22"]
-  private_subnets_B   =["10.0.192.0/21", "10.0.200.0/21", "10.0.208.0/21", "10.0.216.0/21","10.0.224.0/22", "10.0.228.0/22"]
+  source            = "modules/quickstart_vpc"
+  region            = "ap-southeast-2"
+  name              = "${random_pet.name.id}"
+  cidr              = "10.0.0.0/16"
+  public_subnets    = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20", "10.0.240.0/22", "10.0.244.0/22"]
+  private_subnets_A = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19", "10.0.232.0/22", "10.0.236.0/22"]
+  private_subnets_B = ["10.0.192.0/21", "10.0.200.0/21", "10.0.208.0/21", "10.0.216.0/21", "10.0.224.0/22", "10.0.228.0/22"]
 }
 
 ######################################
 # Create Bastion host
 ######################################
 module "bastion" {
-  depends_on = [module.quickstart_vpc]
-  source = "../modules/bastion"
+  depends_on     = [module.quickstart_vpc]
+  source         = "../modules/bastion"
   create_bastion = true
-  region = "ap-southeast-2"
-  name   = "${random_pet.name.id}"
-  key_name = ""
+  region         = "ap-southeast-2"
+  name           = "${random_pet.name.id}"
+  key_name       = ""
 }
