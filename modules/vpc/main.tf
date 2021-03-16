88bfdba32fba62290d012df4024c5b4cabeec69e
Mahesh Raghunandanan <mahrag@amazon.com>
2021-03-16 10:15:16 +0530

    Qucikstart vpc tf files fine tuned

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,7 +4,12 @@
 
 terraform {
   required_version = ">= 0.13"
-
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.10"
+    }
+  }
   backend "remote" {}
 }
 
