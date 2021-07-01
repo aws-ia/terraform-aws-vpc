19b70631973807bfc4ae9d9137a5f3f1330ea69d
Tony Vattathil <avattathil@gmail.com>
2021-06-30 20:40:47 -0700

    map tags

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "aws-vpc" {
   public_outbound_acl_rules = var.public_inbound_acl_rules
   custom_inbound_acl_rules  = var.custom_inbound_acl_rules
   custom_outbound_acl_rules = var.custom_outbound_acl_rules
-  public_subnet_tags        = var.public_subnet_tags
-  private_subnet_tags       = var.private_subnet_tags
+  public_subnet_tags        = tomap(var.public_subnet_tags, { "terraform" = "true" })
+  private_subnet_tags       = tomap(var.private_subnet_tags, { "terraform" = "true" })
 
 }
