e9e6ae0551a8c3e957d2debbba768766b5f8c9e6
Tony Vattathil <avattathil@gmail.com>
2021-06-30 17:53:37 -0700

    Add option to main

--- main.tf
+++ main.tf
@@ -25,12 +25,20 @@ resource "random_pet" "name" {
 ######################################
 
 module "aws-vpc" {
-  source            = "./modules/vpc"
-  region            = var.region
-  name              = random_pet.name.id
-  cidr              = "10.0.0.0/16"
-  public_subnets    = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20", "10.0.240.0/22", "10.0.244.0/22"]
-  private_subnets_A = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19", "10.0.232.0/22", "10.0.236.0/22"]
-  private_subnets_B = ["10.0.192.0/21", "10.0.200.0/21", "10.0.208.0/21", "10.0.216.0/21", "10.0.224.0/22", "10.0.228.0/22"]
-  tags              = var.tags
+  source                    = "./modules/vpc"
+  region                    = var.region
+  name                      = random_pet.name.id
+  cidr                      = var.cidr
+  public_subnets            = var.public_subnets
+  private_subnets_A         = var.private_subnets_A
+  private_subnets_B         = var.private_subnets_B
+  tags                      = var.tags
+  enable_dns_hostnames      = var.enable_dns_hostnames
+  enable_dns_support        = var.enable_dns_support
+  instance_tenancy          = var.instance_tenancy
+  public_inbound_acl_rules  = var.public_inbound_acl_rules
+  public_outbound_acl_rules = var.public_inbound_acl_rules
+  custom_inbound_acl_rules  = var.custom_inbound_acl_rules
+  custom_outbound_acl_rules = var.custom_outbound_acl_rules
+
 }
