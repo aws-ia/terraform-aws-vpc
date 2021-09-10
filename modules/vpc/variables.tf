53c67b547276b26d09e9d4d6d1a92950be1232b9
Jay McConnell <jmmccon@amazon.com>
2021-09-10 13:13:38 -0700

    fix bad merge

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -189,9 +189,3 @@ variable "availability_zones" {
   type        = list(string)
   default     = null
 }
-
-variable "availability_zones" {
-  description = "A list of availability zones to use for subnets"
-  type        = list(string)
-  default     = []
-}
