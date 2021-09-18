baed5b476b91045954201149e41c093efa53f6f4
Farhan Angullia <farhanangullia@gmail.com>
2021-09-18 17:15:54 +0800

    add support for vpc flow logs

--- modules/vpc/locals.tf
+++ modules/vpc/locals.tf
@@ -32,4 +32,12 @@ locals {
   private_b_nacl_count        = length(local.private_subnet_b_cidrs) > 0 ? local.vpc_count : 0
   nat_gateway_private_a_count = var.create_vpc && var.create_igw && var.create_nat_gateways_private_a ? length(local.private_subnet_a_cidrs) : 0
   nat_gateway_private_b_count = var.create_vpc && var.create_igw && var.create_nat_gateways_private_b ? length(local.private_subnet_b_cidrs) : 0
+
+  # flow log variables
+  create_log_group      = var.create_vpc_flow_logs && var.log_destination_type != "s3" && var.log_destination == null
+  create_log_bucket     = var.create_vpc_flow_logs && var.log_destination_type == "s3" && var.log_destination == null
+  create_flow_log_iam_role     = var.create_vpc_flow_logs && var.log_destination_type != "s3" && var.flog_log_iam_role_arn == null
+  log_destination       = (var.create_vpc_flow_logs && local.create_log_group ? aws_cloudwatch_log_group.flow_logs_log_group[0].arn : (local.create_log_bucket ? aws_s3_bucket.flow_logs_bucket[0].arn : var.log_destination))
+  flog_log_iam_role_arn = local.create_flow_log_iam_role ? aws_iam_role.flow_logs_role[0].arn : var.flog_log_iam_role_arn
+  log_format            = var.enriched_meta_data ? "$${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status} $${pkt-src-aws-service} $${pkt-dst-aws-service} $${flow-direction} $${traffic-path}" : var.log_format
 }
