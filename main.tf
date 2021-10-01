30d3044c402953f8d6e0e8219a1b34d529d5aa68
Jay McConnell <jmmccon@amazon.com>
2021-09-30 19:22:01 -0700

    remove unused variables

--- main.tf
+++ main.tf
@@ -8,8 +8,6 @@ terraform {
   }
 }
 
-data "aws_region" "current" {}
-
 data "aws_availability_zones" "available" {
   state = "available"
 }
@@ -20,16 +18,7 @@ locals {
     length(local.private_subnet_a_cidrs),
     length(local.private_subnet_b_cidrs),
   )
-  nat_gateway_count = local.max_subnet_length
 
-  # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
-  vpc_id = element(
-    concat(
-      aws_vpc.main.*.id,
-      [""],
-    ),
-    0,
-  )
   name                   = var.name == null ? length(random_string.vpc_name_suffix) > 0 ? "tf-vpc-${random_string.vpc_name_suffix[0].id}" : "" : var.name
   public_subnet_cidrs    = var.public_subnet_cidrs == null ? cidrsubnets(cidrsubnets(var.cidr, 2)[0], 2, 2, 2) : var.public_subnet_cidrs
   private_subnet_a_cidrs = var.private_subnet_a_cidrs == null ? cidrsubnets(cidrsubnets(var.cidr, 2, 2)[1], 2, 2, 2) : var.private_subnet_a_cidrs
@@ -38,7 +27,6 @@ locals {
 
   # count variables
   vpc_count                   = var.create_vpc ? 1 : 0
-  az_count                    = var.create_vpc ? length(local.availability_zones) : 0
   public_subnet_count         = var.create_vpc ? length(local.public_subnet_cidrs) : 0
   private_subnet_a_count      = var.create_vpc ? length(local.private_subnet_a_cidrs) : 0
   private_subnet_b_count      = var.create_vpc ? length(local.private_subnet_b_cidrs) : 0
