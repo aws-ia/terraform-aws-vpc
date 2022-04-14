27b068aae9110e1b97cb9365610ad306c773efc9
drewmullen <drmullen@amazon.com>
2022-04-14 09:30:01 -0400

    must remove provider blocks from modules: https://github.com/hashicorp/terraform-provider-awscc/issues/466

--- providers.tf
+++ providers.tf
@@ -12,11 +12,3 @@ terraform {
     }
   }
 }
-
-provider "awscc" {
-  user_agent = [{
-    product_name    = "terraform-awscc-vpc"
-    product_version = "0.0.1"
-    comment         = "V1/AWS-D69B4015/376222146"
-  }]
-}
