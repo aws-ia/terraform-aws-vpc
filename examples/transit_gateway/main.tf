473047d4d29f13b04f1e5c82411cbc441409a8c5
drewmullen <drmullen@amazon.com>
2022-05-03 14:23:52 -0400

    intended argument structure

--- examples/transit_gateway/main.tf
+++ examples/transit_gateway/main.tf
@@ -11,9 +11,23 @@ module "vpc" {
   az_count   = 2
 
   subnets = {
+    public = {
+      netmask = 24
+      route_to_transit_gateway = ["0.0.0.0/0"]
+    }
+
+    private = {
+      netmask = 24
+      route_to_nat = true
+      route_to_transit_gateway = ["0.0.0.0/0"]
+    }
+
     transit_gateway = {
       netmask            = 24
       transit_gateway_id = aws_ec2_transit_gateway.example.id
+      route_to_nat = true # default false
+      transit_gateway_default_route_table_association = true
+      transit_gateway_default_route_table_propagation = true
     }
   }
 }
