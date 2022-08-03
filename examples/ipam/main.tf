3b1f34b04b52b6affbdf2eaa221cbf0e7b0f8f26
drewmullen <drmullen@amazon.com>
2022-08-03 08:40:41 -0400

    example formatting

--- examples/ipam/main.tf
+++ examples/ipam/main.tf
@@ -5,13 +5,12 @@ module "vpc" {
   name     = "ipam-vpc"
   az_count = 3
 
-  vpc_ipv4_ipam_pool_id   = module.ipam_base_for_example_only.pool_id # var.ipam_pool_id
+  vpc_ipv4_ipam_pool_id   = var.ipam_pool_id
   vpc_ipv4_netmask_length = 20
 
   subnets = {
     public = {
-      netmask = 24
-
+      netmask                   = 24
       nat_gateway_configuration = "all_azs"
     }
     private = {
