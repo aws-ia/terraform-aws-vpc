90040a9be9d2ac637d8d0311dec4a0f070282f15
Vivek Goyal <vivgoyal@amazon.com>
2022-09-12 20:40:30 -0700

    Verified new Terratest module

--- test/examples_nat_gw_routes_test.go
+++ test/examples_nat_gw_routes_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/likexian/gokit/assert"
 )
 
 
@@ -13,9 +12,9 @@ func TestExamplesNATGWRoutesNoNATGWNoRoute(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/nat_gw_routes",
 		Vars: map[string]interface{}{
-			"nat_gateway_configuration" : "none"
-			"route_to_nw" : false
-		}
+			"nat_gateway_configuration" : "none",
+			"route_to_nw" : false,
+		},
 	}
 
 	defer terraform.Destroy(t, terraformOptions)
@@ -28,9 +27,9 @@ func TestExamplesNATGWRoutesSingleAZNATGWNoRoute(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/nat_gw_routes",
 		Vars: map[string]interface{}{
-			"nat_gateway_configuration" : "single_az"
-			"route_to_nw" : false
-		}
+			"nat_gateway_configuration" : "single_az",
+			"route_to_nw" : false,
+		},
 	}
 
 	defer terraform.Destroy(t, terraformOptions)
@@ -43,9 +42,9 @@ func TestExamplesNATGWRoutesAllAZsNATGWNoRoute(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/nat_gw_routes",
 		Vars: map[string]interface{}{
-			"nat_gateway_configuration" : "all_azs"
-			"route_to_nw" : false
-		}
+			"nat_gateway_configuration" : "all_azs",
+			"route_to_nw" : false,
+		},
 	}
 
 	defer terraform.Destroy(t, terraformOptions)
@@ -58,9 +57,9 @@ func TestExamplesNATGWRoutesSingleAZNATGWWithRoute(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/nat_gw_routes",
 		Vars: map[string]interface{}{
-			"nat_gateway_configuration" : "single_az"
-			"route_to_nw" : true
-		}
+			"nat_gateway_configuration" : "single_az",
+			"route_to_nw" : true,
+		},
 	}
 
 	defer terraform.Destroy(t, terraformOptions)
@@ -73,9 +72,9 @@ func TestExamplesNATGWRoutesAllAZsNATGWWithRoute(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/nat_gw_routes",
 		Vars: map[string]interface{}{
-			"nat_gateway_configuration" : "all_azs"
-			"route_to_nw" : true
-		}
+			"nat_gateway_configuration" : "all_azs",
+			"route_to_nw" : true,
+		},
 	}
 
 	defer terraform.Destroy(t, terraformOptions)
