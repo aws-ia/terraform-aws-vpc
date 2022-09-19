96816ef4107417d19cac20342cff300d919ff08e
drewmullen <drmullen@amazon.com>
2022-09-19 16:00:27 -0400

    ipam test updates

--- examples/ipam/main.tf
+++ examples/ipam/main.tf
@@ -1,31 +1,27 @@
 module "vpc" {
   # source  = "aws-ia/vpc/aws"
-  # version = ">= 2.0.0"
-  # source = "github.com/vivgoyal-aws/terraform-aws-vpc"
-
+  # version = ">= 3.0.0"
   source = "../.."
 
   name     = "ipam-vpc"
   az_count = 3
 
-  vpc_ipv4_ipam_pool_id = "ipam-pool-079f76df39be519c9" # module.ipam_base_for_example_only.pool_id #var.ipam_pool_id #
-  # vpc_ipv4_netmask_length = 26
-  cidr_block = "172.2.0.192/26"
+  vpc_ipv4_ipam_pool_id   = module.ipam_base_for_example_only.pool_id
+  vpc_ipv4_netmask_length = 26
 
   subnets = {
     private = {
-      cidrs = ["172.2.0.192/28", "172.2.0.208/28", "172.2.0.224/28"]
-      # netmask                 = 28
+      netmask                 = 28
       connect_to_public_natgw = false
     }
   }
 }
 
 #####################################
+# For example purposes ONLY!
 # Example of a simple IPAM deployment
-# terraform apply -target=module.ipam_base_for_example_only
 #####################################
 
-# module "ipam_base_for_example_only" {
-#   source = "../../test/hcl_fixtures/ipam_base"
-# }
+module "ipam_base_for_example_only" {
+  source = "../../test/hcl_fixtures/ipam_base"
+}
--- test/examples_ipam_test.go
+++ test/examples_ipam_test.go
@@ -7,19 +7,10 @@ import (
 )
 
 func TestExamplesIPAM(t *testing.T) {
-
-	ipamBase := &terraform.Options{
-		TerraformDir: "./hcl_fixtures/ipam_base",
-	}
-	defer terraform.Destroy(t, ipamBase)
-	terraform.InitAndApply(t, ipamBase)
-
-	pool_id := terraform.Output(t, ipamBase, "pool_id")
-
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/ipam",
 		Vars: map[string]interface{}{
-			"ipam_pool_id": pool_id,
+			"ipam_pool_id": "test",
 		},
 	}
 
