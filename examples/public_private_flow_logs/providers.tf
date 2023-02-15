88d4c80ae7b63334c04d892c5c075e21a697cf6a
drewmullen <drmullen@amazon.com>
2023-02-15 15:45:52 -0500

    rm required_provider refs

--- examples/public_private_flow_logs/providers.tf
+++ examples/public_private_flow_logs/providers.tf
@@ -5,17 +5,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.73.0"
     }
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = ">= 0.35.0"
-    }
-  }
-}
-
-provider "aws" {
-  default_tags {
-    tags = {
-      "key" = "value"
-    }
   }
 }
