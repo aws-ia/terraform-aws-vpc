8d9c198e8997edbeaeec96d2cabf2c911744b71c
Pablo Sanchez Carmona <pablosc@amazon.es>
2023-05-03 20:27:43 +0200

    fixing tfsec findings

--- examples/vpc_lattice/main.tf
+++ examples/vpc_lattice/main.tf
@@ -56,10 +56,19 @@ resource "aws_security_group" "security_group" {
   }
 
   egress {
+    description = "Allowing inter-VPC traffic."
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  egress {
+    description = "Allowing VPC Lattice traffic."
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["169.254.171.0/24"]
   }
 }
 
