0e60b1f175cfda7e3d566d822bea5bfbc5e99fb0
drewmullen <mullen.drew@gmail.com>
2023-07-25 18:38:54 -0400

    try with plan and locals

--- tests/basic/main.tf
+++ tests/basic/main.tf
@@ -2,3 +2,9 @@
 resource "aws_vpc" "name" {
   cidr_block = "10.0.0.0/16"
 }
+
+locals {
+  myval = "drew"
+}
+
+
---
+++ tests/basic/outputs.tf
@@ -0,0 +1,4 @@
+output "name" {
+  description = "value"
+  value       = local.myval
+}
